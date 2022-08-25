feebb701803349f9e2c4dfff0722f1f5533ede36
Weverson Diazz <63312710+wevdiaz@users.noreply.github.com>
2022-08-25 16:39:33 -0300

    conexão com o banco de dados

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const routes = require("./routes");
+require("dotenv").config();
 
 
 app.use( express.urlencoded({ extended: true }));
@@ -8,8 +10,15 @@ app.use(express.json());
 
 app.use(routes);
 
+mongoose.connect(`mongodb+srv://wevdiazz:${process.env.CONNECTIONDB}@clusterdb.p4ohbir.mongodb.net/bancodaapi?retryWrites=true&w=majority`)
+  .then(() => {
+    console.log("Conectamos ao banco MongoDB");
 
-app.listen(3000, () => {
-  console.log("http://localhost:3000");
-  console.log("Server is running!");
-});
+    app.listen(3000, () => {
+      console.log("http://localhost:3000");
+      console.log("Server is running!");
+    });
+
+  })
+  .catch((err) => console.log(err));
+
